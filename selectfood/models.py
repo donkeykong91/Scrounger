d726c6d4a2e4ee2a2be6be25667ff4481bc73c04
Daryl Occ <darylnator@gmail.com>
2017-05-26 23:31:59 -0700

    deleted old db and made new db with foreign key

--- selectfood/models.py
+++ selectfood/models.py
@@ -9,6 +9,6 @@ class Food(models.Model):
 class Event(models.Model):
     event_date_time = models.DateTimeField()
     event_location = models.CharField(max_length=20)
-    food_id = models.ForeignKey(Food, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
     def __str__(self):
         return self.event_location
