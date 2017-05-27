e3cfdd2bcf256b53f6980ff2d827b7a3a2ec3ca5
Daryl Occ <darylnator@gmail.com>
2017-05-27 13:53:51 -0700

    food links work and return location and time of food

--- selectfood/urls.py
+++ selectfood/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'selectfood'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.EventView.as_view(), name='events'),
 ]
--- selectfood/views.py
+++ selectfood/views.py
@@ -5,5 +5,9 @@ from django.utils import timezone
 from .models import Food, Event
 
 class IndexView(generic.ListView):
-    template_name = 'selectfood/index.html'
     model = Food
+    template_name = 'selectfood/index.html'
+
+class EventView(generic.DetailView):
+    model = Food
+    template_name = 'selectfood/events.html'
